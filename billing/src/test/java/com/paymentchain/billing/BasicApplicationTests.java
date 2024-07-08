d29fad149874252f8fc48686e8d77bebc6d90744
JNavarro <JNavarro@gmail.com>
2024-07-08 11:29:52 -0400

    Cambio para prueba de pipeline 2

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
  
  @Test
  public void contextLoads() {
- 	    String meessage= "default message cambio test devops jenkins: ci/cd..";
+ 	    String meessage= "default message cambio test devops jenkins:: ci/cd..";
  	    Assert.assertNotNull(meessage);
  }
  
